c70ce1a72250065b573fc0a82cd3b621fdf94117
gochim77 <tu7pham7@gmail.com>
2023-11-18 23:28:41 +0700

    deleted:    Table.py 	modified:   bot.cpp 	new file:   game.cpp 	new file:   main.cpp 	deleted:    main.py 	new file:   minimax.cpp 	deleted:    variables.py

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include "minimax.cpp"
+
+int main()
+{    
+	vector<ar<int, 2> > state(12);
+	int turn, P1points, P2points;
+	cin >> turn;
+	for (int i = 0; i < 12; i++) cin >> state[i][0] >> state[i][1];
+	cin >> P1points >> P2points;
+
+	// Game game(0, {{0, 1}, {5, 0}, {5, 0}, {5, 0}, {5, 0}, {5, 0}, {0, 1}, {5, 0}, {5, 0}, {5, 0}, {5, 0}, {5, 0}}, 0, 0);
+    Game game(turn, state, P1points, P2points);
+    MinimaxStrategy minimax9(&game, 9);
+    minimax.calculate_move().print();
+    
+    // Player p1;
+    // p1.set_strategy(&minimax1);
+    // Player p2;
+    // p2.set_strategy(&minimax1);
+
+    // int num_turn = 1;
+    // while(!game.check_ending())
+    // {
+    // 	// game.check_borrow(game.turn);
+    // 	cout << "TURN: " << num_turn << '\n';
+    // 	game.print_table();
+    // 	if (game.turn == 0) p1.play();
+    // 	else p2.play();
+    // 	num_turn++;
+    // 	// cout << game.turn << '\n';
+    // }
+    // game.print_table();
+    // int v = game.check_ending();
+    // if (v == 1) cout << "P1 wins";
+    // else if (v == 2) cout << "p2 wins";
+    // else if (v == 3) cout << "draw";
+    return 0;
+}
---
+++ minimax.cpp
@@ -0,0 +1,78 @@
+#include "game.cpp"
+
+class MinimaxStrategy : public Strategy {
+public:
+	const int INF = 7777;
+	vector<Move> bestLine;
+	int MAX_DEPTH;
+    MinimaxStrategy(Game* game, int MAX_DEPTH) : Strategy(game), MAX_DEPTH(MAX_DEPTH) {}
+
+    Move calculate_move() override 
+    {
+        Move bestMove = {-1, -1};
+        int bestValue = (game->turn == 0) ? -INF : INF; 
+        int alpha = -INF, beta = INF;
+        // cout << "CALCULATION: \n";
+        int isMaximizingPlayer = !(game->turn);
+        for (Move move : game->possible_move()) 
+        {
+            Game tempGame = *game;
+            tempGame.make_move(move);
+            int moveValue = minimax(tempGame, 0, !isMaximizingPlayer, alpha, beta);
+            if (optimize(bestValue, moveValue, isMaximizingPlayer)) bestMove = move;
+            if (isMaximizingPlayer) alpha = max(alpha, moveValue);
+            else beta = min(beta, moveValue);
+            if (beta <= alpha) break;
+        }
+        // cout << "MOVE PLAYED: ";
+    	// bestMove.print();
+        return bestMove;
+    }
+
+private:
+	int num_state = 0;
+	map<int, Move> track;
+	bool optimize(int& best, int value, bool isMaximizingPlayer)
+    {
+    	if (isMaximizingPlayer) // if maximize player
+    	{
+    		if (best < value) return best = value, 1;
+    	}
+    	else if (best > value) return best = value, 1;
+    	return 0;
+    }
+
+	int ultility(Game& game, int isEnd)
+	{
+		if (isEnd == 1) return INF - 1;
+		else if (isEnd == 2) return -INF + 1;
+		return game.P1points - game.P2points;
+	}
+
+    int minimax(Game& game, int depth, bool isMaximizingPlayer, int& alpha, int& beta)
+    {
+    	int isEnd = game.check_ending();
+        if (isEnd || depth == MAX_DEPTH) return ultility(game, isEnd);
+             
+        int bestValue = isMaximizingPlayer ? -INF : INF;  
+        Move bestMove = {-1, -1};
+        
+        for (Move move : game.possible_move()) 
+       	{
+            Game tempGame = game;
+            tempGame.make_move(move);     
+            // cout << "P1 move: ";
+            // move.print();
+            // tempGame.print_table();
+            int moveValue = minimax(tempGame, depth + 1, !isMaximizingPlayer, alpha, beta);
+            // cout << moveValue << endl;
+            if (optimize(bestValue, moveValue, isMaximizingPlayer)) bestMove = move;
+            
+            if (isMaximizingPlayer) alpha = max(alpha, bestValue);
+            else beta = min(beta, bestValue);
+            if (beta <= alpha) break;
+    	}	
+
+        return bestValue;
+    }
+};
